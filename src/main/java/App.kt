d1098724de3f5e11f7d2e97a0ba50646e732f541
Zahar Zolotarev <zahar.zolotarev@gmail.com>
2016-12-04 17:08:32 +0300

    temp

--- src/main/java/App.kt
+++ src/main/java/App.kt
@@ -1,5 +1,5 @@
-import alho.Bisection
-import alho.IAlgorithm
+import alho.*
+import kotlin.reflect.KFunction2
 
 /**
  * Created by Zahar on 02.11.16.
@@ -9,19 +9,61 @@ import alho.IAlgorithm
 
 
 fun main(args: Array<String>) {
+//
+//	val E = Math.pow(10.0,-12.0)
+//
+//	val expression: (Double) -> Double = {
+//		Math.tan(it + Math.PI / 5)
+//	}
+//
+//	println("Метод половинного деления")
+//	val result = Bisection().find(1.0, 4.0, E, expression)
+//	qwer(expression, result)
+//
+//	println()
+//	println("Метод Ньютона")
+//	val derivative: (Double) -> Double = {
+//		val cos = Math.cos(it + Math.PI / 5)
+//		1 /(cos * cos)
+//	}
+//	val result2 = Newton().find(3.0, E, expression,derivative)
+//	qwer(expression, result2)
+//
+//	println()
+//
+//	println("Метод простой итерации")
+//	val result3 = Simple().find(3.0, E, expression)
+//	qwer(expression, result3)
+		val E = Math.pow(10.0,-12.0)
 
-	val E = Math.pow(10.0,-12.0)
+	Grad().work(::f, )
 
-	val a: IAlgorithm = Bisection()
+}
 
-	val expression: (Double) -> Double = {
-		Math.tan(it + Math.PI / 5)
+class Grad {
+	fun work(kFunction2: KFunction2<Double, Double, Double>) {
+		val y = 0.0
+		var yNext = y + kFunction2()
+
 	}
-	val result = a.find(1.0, 4.0, E, expression)
+}
+
+class NewToon {
+	fun work(kFunction2: KFunction2<Double, Double, Double>) {
+		val y = 0.0
+		var yNext = y + kFunction2()
+
+	}
+}
+
+fun f(x1:Double,x2:Double): Double {
+	val d1 = x2 - 1
+	return x1*x1+10*d1*d1
+}
 
 
+private fun qwer(expression: (Double) -> Double, result: CalcResult) {
 	println("F(x): = tg(x + PI/5)")
 	println("Результат: $result")
 	println("F(${result.point}): =${expression.invoke(result.point)}")
-
 }
--- src/main/java/alho/IAlgorithm.kt
+++ src/main/java/alho/IAlgorithm.kt
@@ -8,22 +8,17 @@ interface IAlgorithm {
 	fun find(left: Double, right: Double, accuracy: Double, expression: (Double) -> Double): CalcResult
 }
 
-data class CalcResult(val iterationCount: Int,val point: Double, val error:Boolean = false)
+data class CalcResult(val iterationCount: Int, val point: Double, val error: Boolean = false)
 
 
 class Bisection(val maxIteration: Int = 1000) : IAlgorithm {
 	override fun find(pleft: Double, pright: Double, accuracy: Double, expression: (Double) -> Double): CalcResult {
-
-
-
-
 		var iterationCount = 0
 		var right = pright
 		var left = pleft
 
-		if (expression(left) * expression(right) >= 0)
-		{
-			CalcResult(iterationCount, 0.0,true)
+		if (expression(left) * expression(right) >= 0) {
+			CalcResult(iterationCount, 0.0, true)
 		}
 
 		var c: Double
@@ -44,5 +39,50 @@ class Bisection(val maxIteration: Int = 1000) : IAlgorithm {
 		} while (true)
 
 		return CalcResult(iterationCount, c)
+	}
+}
+
+class Newton {
+
+	fun find(px: Double, accuracy: Double, expression: (Double) -> Double, derivative: (Double) -> Double): CalcResult {
+		var iterationCount = 0
+		var x1: Double
+		var y: Double
+		var x = px
+		do {
+			iterationCount++
+			val ext = expression(x)
+			x1 = x - ext / derivative(x)
+			x = x1
+			y = ext
+		} while (Math.abs(y) >= accuracy)
+
+		return CalcResult(iterationCount, x)
+	}
+}
+
+class Simple {
+
+	fun find(px: Double, accuracy: Double, expression: (Double) -> Double): CalcResult {
+		var x = 0.0
+		var xPrev = px
+
+		var iterationCount = 0
+		do {
+			val expressionPrev = expression(xPrev)
+			if (Math.signum(expression(x)) == Math.signum(expressionPrev)) {
+				x = xPrev - 1.2 * expressionPrev
+			} else {
+				x = xPrev - 0.5 * expressionPrev
+			}
+
+			if (Math.abs(x - xPrev) <= accuracy || iterationCount == 1000) {
+				break
+			}
+			xPrev = x
+			iterationCount++
+		} while (true)
+
+		return CalcResult(iterationCount, x)
 	}
 }
