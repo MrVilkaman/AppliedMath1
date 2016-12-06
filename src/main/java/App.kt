c1759f80d376e688ac511d8375980738c60fe93d
Zahar Zolotarev <zahar.zolotarev@gmail.com>
2016-12-07 00:50:53 +0300

    lab 2

--- src/main/java/App.kt
+++ src/main/java/App.kt
@@ -6,64 +6,27 @@ import kotlin.reflect.KFunction2
  */
 
 
-
-
 fun main(args: Array<String>) {
-//
-//	val E = Math.pow(10.0,-12.0)
-//
-//	val expression: (Double) -> Double = {
-//		Math.tan(it + Math.PI / 5)
-//	}
-//
-//	println("Метод половинного деления")
-//	val result = Bisection().find(1.0, 4.0, E, expression)
-//	qwer(expression, result)
-//
-//	println()
-//	println("Метод Ньютона")
-//	val derivative: (Double) -> Double = {
-//		val cos = Math.cos(it + Math.PI / 5)
-//		1 /(cos * cos)
-//	}
-//	val result2 = Newton().find(3.0, E, expression,derivative)
-//	qwer(expression, result2)
-//
-//	println()
-//
-//	println("Метод простой итерации")
-//	val result3 = Simple().find(3.0, E, expression)
-//	qwer(expression, result3)
-		val E = Math.pow(10.0,-12.0)
 
-	Grad().work(::f, )
+	val E = Math.pow(10.0, -12.0)
 
+	val grad = Grad().work(::f, ::gradX1, ::gradX2, E, false)
+	val grad2 = GradFast().work(::f, ::gradX1, ::gradX2, E, false)
+	val grad3 = Newtoon().work(::f, ::gradX1, ::gradX2, E, false)
+
+	qwer(grad)
+	qwer(grad2)
+	qwer(grad3)
 }
 
-class Grad {
-	fun work(kFunction2: KFunction2<Double, Double, Double>) {
-		val y = 0.0
-		var yNext = y + kFunction2()
-
-	}
-}
-
-class NewToon {
-	fun work(kFunction2: KFunction2<Double, Double, Double>) {
-		val y = 0.0
-		var yNext = y + kFunction2()
-
-	}
-}
-
-fun f(x1:Double,x2:Double): Double {
-	val d1 = x2 - 1
-	return x1*x1+10*d1*d1
+private fun qwer(grad: Ans) {
+	println("f= ${f(grad.x, grad.y).format()}| x1 = ${grad.x.format()} x2 = ${grad.y.format()} за ${grad.k}")
 }
 
 
-private fun qwer(expression: (Double) -> Double, result: CalcResult) {
-	println("F(x): = tg(x + PI/5)")
-	println("Результат: $result")
-	println("F(${result.point}): =${expression.invoke(result.point)}")
-}
+fun f(x1: Double, x2: Double): Double = x1 * x1 + 10 * (x2 - 1) * (x2 - 1)
+
+fun gradX1(x1: Double, x2: Double): Double = 2 * x1
+
+fun gradX2(x1: Double, x2: Double): Double = 20 * x2 - 20
+
--- src/main/java/alho/IAlgorithm.kt
+++ src/main/java/alho/IAlgorithm.kt
@@ -1,88 +1,185 @@
 package alho
 
+import f
+import gradX1
+import gradX2
+import kotlin.reflect.KFunction2
+
 /**
  * Created by Zahar on 02.11.16.
  */
 
-interface IAlgorithm {
-	fun find(left: Double, right: Double, accuracy: Double, expression: (Double) -> Double): CalcResult
+data class Ans(val x: Double, val y: Double, val k: Int)
+
+fun Double.format() = this.format(13)
+fun Double.format(digits: Int) = java.lang.String.format("%.${digits}f", this)
+
+
+class Newtoon {
+	fun work(func: KFunction2<Double, Double, Double>, gradX: KFunction2<Double, Double, Double>,
+	         gradY: KFunction2<Double, Double, Double>, e: Double, log: Boolean): Ans {
+
+		val x0 = 4.12
+		val y0 = 2.74
+		var x = x0
+		var y = y0
+
+		var gradxPrev = 0.0
+		var gradyPrev = 0.0
+
+		var k = 0
+		if (log)
+			output(x, y, k)
+		do {
+
+			val gradx = gradX(x, y)
+			val grady = gradY(x, y)
+			x -= getGesX() * gradx
+			y -= getGesY() * grady
+
+
+			val d = gradx - gradxPrev
+			val d2 = grady - gradyPrev
+			val prev = Math.sqrt(d * d + d2 * d2)
+			gradxPrev = gradx
+			gradyPrev = grady
+			k++
+
+			if (log)
+				output(x, y, k)
+		} while (e < prev)
+
+		return Ans(x, y, k)
+	}
+
+	private fun  getGesX(): Double = 20.0/40.0
+	private fun  getGesY(): Double = 2.0/40.0
 }
 
-data class CalcResult(val iterationCount: Int, val point: Double, val error: Boolean = false)
+
+class Grad {
+	fun work(func: KFunction2<Double, Double, Double>, gradX: KFunction2<Double, Double, Double>,
+	         gradY: KFunction2<Double, Double, Double>, e: Double, log: Boolean): Ans {
+
+		val x0 = 4.12
+		val y0 = 2.74
+		val h = 0.05
+		var x = x0
+		var y = y0
+
+		var gradxPrev = 0.0
+		var gradyPrev = 0.0
+
+		var k = 0
+		if (log)
+			output(x, y, k)
+		do {
+
+			val gradx = gradX(x, y)
+			val grady = gradY(x, y)
+			x -= h * gradx
+			y -= h * grady
 
 
-class Bisection(val maxIteration: Int = 1000) : IAlgorithm {
-	override fun find(pleft: Double, pright: Double, accuracy: Double, expression: (Double) -> Double): CalcResult {
-		var iterationCount = 0
-		var right = pright
-		var left = pleft
+			val d = gradx - gradxPrev
+			val d2 = grady - gradyPrev
+			val prev = Math.sqrt(d * d + d2 * d2)
+			gradxPrev = gradx
+			gradyPrev = grady
+			k++
 
-		if (expression(left) * expression(right) >= 0) {
-			CalcResult(iterationCount, 0.0, true)
+			if (log)
+				output(x, y, k)
+		} while (e < prev)
+
+		return Ans(x, y, k)
+	}
+
+
+}
+
+fun output(x: Double, y: Double, k: Int) {
+	println("$k) ${x.format()} ${y.format()}")
+
+}
+
+class GradFast {
+	fun work(func: KFunction2<Double, Double, Double>, gradX: KFunction2<Double, Double, Double>,
+	         gradY: KFunction2<Double, Double, Double>, e: Double, log: Boolean): Ans {
+
+		val x0 = 4.12
+		val y0 = 2.74
+		var x = x0
+		var y = y0
+
+		var gradxPrev = 0.0
+		var gradyPrev = 0.0
+
+		var k = 0
+		if (log)
+			output(x, y, k)
+		do {
+
+			val gradx = gradX(x, y)
+			val grady = gradY(x, y)
+			val alpha = Dihotomia(-1000.0,1000.0,0.0001,x,y)
+			x -= alpha * gradx
+			y -=  alpha * grady
+
+
+			val d = gradx - gradxPrev
+			val d2 = grady - gradyPrev
+			val prev = Math.sqrt(d * d + d2 * d2)
+			gradxPrev = gradx
+			gradyPrev = grady
+			k++
+
+			if (log)
+				output(x, y, k)
+		} while (e < prev)
+
+		return Ans(x, y, k)
+	}
+}
+
+fun g(x: Double, y: Double, alpha: Double): Double {
+	return f(x - alpha * gradX1(x, y), y - alpha * gradX2(x, y))
+}
+
+//Метод половинного деления для нахождения минимума в градиентном спуске
+fun Dihotomia(a0: Double, b0: Double, epsilon: Double, x: Double, y: Double): Double {
+	//Номер шага
+	var k: Int
+	//Отклонени от середины отрезка влево, вправо
+	var lk: Double
+	var mk: Double
+	//Величина на которую мы отклонимся от середины отрезка
+	val delta = 0.5 * epsilon
+	//Точка минимума
+	val x_: Double
+	//Отрезок локализации минимума
+	var ak = a0
+	var bk = b0
+	k = 1
+	//Пока длина отрезка больше заданной точности
+	do {
+		//Берем середину (ну почти середину - +\- некоторое дельта в частности у нас delta=0.5*epsilon)
+		lk = (ak + bk - delta) / 2
+		mk = (ak + bk + delta) / 2
+
+		k++
+		//Проверяем в какую часть попадает точка минимума слева от разбиения или справа и выбираем соответствующую точку
+		if (g(x, y, lk) <= g(x, y, mk)) {
+			//Теперь правая граница отрезка локализации равна mk
+			bk = mk
+		} else {
+			//Теперь левая граница отрезка локализации равна mk
+			ak = lk
 		}
+	} while (bk - ak >= epsilon)
 
-		var c: Double
-		do {
-			c = (left + right) / 2;
+	x_ = (ak + bk) / 2 //minimum point
 
-			if (expression(left) * expression(c) < 0) {
-				right = c
-			} else {
-				left = c
-			}
-
-			if (Math.abs(expression(c)) <= accuracy || (right - left) <= accuracy || iterationCount == maxIteration) {
-				break
-			}
-
-			iterationCount++
-		} while (true)
-
-		return CalcResult(iterationCount, c)
-	}
+	return x_
 }
 
-class Newton {
-
-	fun find(px: Double, accuracy: Double, expression: (Double) -> Double, derivative: (Double) -> Double): CalcResult {
-		var iterationCount = 0
-		var x1: Double
-		var y: Double
-		var x = px
-		do {
-			iterationCount++
-			val ext = expression(x)
-			x1 = x - ext / derivative(x)
-			x = x1
-			y = ext
-		} while (Math.abs(y) >= accuracy)
-
-		return CalcResult(iterationCount, x)
-	}
-}
-
-class Simple {
-
-	fun find(px: Double, accuracy: Double, expression: (Double) -> Double): CalcResult {
-		var x = 0.0
-		var xPrev = px
-
-		var iterationCount = 0
-		do {
-			val expressionPrev = expression(xPrev)
-			if (Math.signum(expression(x)) == Math.signum(expressionPrev)) {
-				x = xPrev - 1.2 * expressionPrev
-			} else {
-				x = xPrev - 0.5 * expressionPrev
-			}
-
-			if (Math.abs(x - xPrev) <= accuracy || iterationCount == 1000) {
-				break
-			}
-			xPrev = x
-			iterationCount++
-		} while (true)
-
-		return CalcResult(iterationCount, x)
-	}
-}
